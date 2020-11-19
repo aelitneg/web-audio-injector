e2cc16f2e149d5a1ec59946ff20bf5c1f5fce19b
Andrew Gentile <agentile1990@gmail.com>
2020-11-19 07:57:15 -0600

    Setup Docker Container for Client

---
+++ client/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: false,
+        node: true,
+        es6: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {},
+};
